6dae6f2d891cf232f006c8b4e7756b5347607014
wangkun <wb-wk228937@ant-financial.com>
2017-03-31 18:04:46 +0800

    try browser-sync for mock server

--- proxy/index.js
+++ proxy/index.js
@@ -1,23 +1,42 @@
+var path = require('path');
 var projectConfig = require('../config/project.js');
-var api = require('../src/service/api.js');
-var lodash = require('lodash');
-var express = require('express');
-var app = express();
+// var api = require('../src/service/api.js');
+// var lodash = require('lodash');
+// var express = require('express');
+// var app = express();
 var __SERVER__ = projectConfig.devServer;
 
-Object.keys(api).forEach(function (item, i) {
-    var obj = api[item];
-    var body = require(`./proxy${lodash.capitalize(item)}.js`);
-    if (obj.open && body) {
-        app.use(obj.url, function (req, res, next) {
-            res.json(body);
-        })
-    }
-})
+// Object.keys(api).forEach(function (item, i) {
+//     var obj = api[item];
+//     var body = require(`./proxy${lodash.capitalize(item)}.js`);
+//     if (obj.open && body) {
+//         app.use(obj.url, function (req, res, next) {
+//             res.status(200).json(body).end();
+//         })
+//     }
+// })
 
-var server = app.listen(__SERVER__.port + 1, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+// app.use('/api', express.static('./'));
 
-    console.log('localhost proxy server is listening at http://%s:%s', host, port);
+// var server = app.listen(__SERVER__.port + 1, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+
+//     console.log('localhost proxy server is listening at http://%s:%s', host, port);
+// });
+
+var browserSync = require("browser-sync").create();
+
+browserSync.init({
+    server: {
+        baseDir: "./proxy/",
+        directory: true
+    },
+    ui: {
+        port: __SERVER__.port + 2
+    },
+    open: false,
+    online: false,
+    codeSync: true,
+    port: __SERVER__.port + 1
 });
